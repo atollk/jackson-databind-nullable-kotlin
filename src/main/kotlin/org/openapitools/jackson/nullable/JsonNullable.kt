72b41e3af2c8ea3031793c3fce0bcdd672da2f2d
Andreas Tollkötter <andreas.tollkoetter@flipapp.de>
2023-05-08 11:09:23 +0200

    Switched JsonNullable to data class

--- src/main/kotlin/org/openapitools/jackson/nullable/JsonNullable.kt
+++ src/main/kotlin/org/openapitools/jackson/nullable/JsonNullable.kt
@@ -2,21 +2,19 @@ package org.openapitools.jackson.nullable
 
 import java.io.Serializable
 import java.util.*
-import java.util.function.Consumer
 
-class JsonNullable<T> private constructor(private val value: T, val isPresent: Boolean) : Serializable {
+data class JsonNullable<T>(private val value: T, val isPresent: Boolean) : Serializable {
     /**
      * Obtain the value of this `JsonNullable`.
      *
      * @return the value, if present
      * @throws NoSuchElementException if no value is present
      */
-    fun get(): T {
-        if (!isPresent) {
+    fun get(): T =
+        if (!isPresent)
             throw NoSuchElementException("Value is undefined")
-        }
-        return value
-    }
+        else
+            value
 
     /**
      * Obtain the value of this `JsonNullable`.
@@ -24,9 +22,8 @@ class JsonNullable<T> private constructor(private val value: T, val isPresent: B
      * @param other the value to be returned if no value is present
      * @return the value of this `JsonNullable` if present, the submitted value otherwise
      */
-    fun orElse(other: T): T {
-        return if (isPresent) value else other
-    }
+    fun orElse(other: T): T =
+        if (isPresent) value else other
 
     /**
      * If a value is present, performs the given action with the value,
@@ -35,27 +32,11 @@ class JsonNullable<T> private constructor(private val value: T, val isPresent: B
      * @param action the action to be performed, if a value is present
      */
     fun ifPresent(
-        action: Consumer<in T>
+        action: (T) -> Unit
     ) {
         if (isPresent) {
-            action.accept(value)
+            action(value)
         }
-    }
-
-    override fun equals(obj: Any?): Boolean {
-        if (this === obj) {
-            return true
-        }
-        if (obj !is JsonNullable<*>) {
-            return false
-        }
-        val other = obj
-        return value == other.value &&
-                isPresent == other.isPresent
-    }
-
-    override fun hashCode(): Int {
-        return Objects.hash(value, isPresent)
     }
 
     override fun toString(): String {
@@ -72,9 +53,7 @@ class JsonNullable<T> private constructor(private val value: T, val isPresent: B
          * @param <T> a type wildcard
          * @return an empty `JsonNullable` with no value defined
         </T> */
-        fun <T> undefined(): JsonNullable<T> {
-            return UNDEFINED as JsonNullable<T>
-        }
+        fun <T> undefined(): JsonNullable<T> = UNDEFINED as JsonNullable<T>
 
         /**
          * Create a `JsonNullable` from the submitted value.
@@ -83,8 +62,6 @@ class JsonNullable<T> private constructor(private val value: T, val isPresent: B
          * @param <T>   the type of the value
          * @return the `JsonNullable` with the submitted value present.
         </T> */
-        fun <T> of(value: T): JsonNullable<T> {
-            return JsonNullable(value, true)
-        }
+        fun <T> of(value: T): JsonNullable<T> = JsonNullable(value, true)
     }
 }
