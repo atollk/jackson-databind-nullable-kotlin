f33d4b2b9beea6c974cbbbb9f8731a4941854e5e
Andreas Tollkötter <andreas.tollkoetter@flipapp.de>
2023-05-08 10:44:46 +0200

    Configured Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     `java-library`
     `maven-publish`
+    kotlin("jvm") version "1.8.21"
 }
 
 repositories {
@@ -12,6 +15,7 @@ repositories {
     maven {
         url = uri("https://repo.maven.apache.org/maven2/")
     }
+    mavenCentral()
 }
 
 dependencies {
@@ -42,4 +46,14 @@ tasks.withType<JavaCompile>() {
 
 tasks.withType<Javadoc>() {
     options.encoding = "UTF-8"
+}
+
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
 }
